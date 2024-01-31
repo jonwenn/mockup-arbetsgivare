de776a01ccb2e401aefff1454e45dd81cdc879f9
Jonas <jonwenn@gmail.com>
2024-01-31 15:19:34 +0100

    Update

--- MockupRedoPeriod/MockupRedoPeriod/obj/Debug/net8.0/MockupRedoPeriod.AssemblyInfo.cs
+++ MockupRedoPeriod/MockupRedoPeriod/obj/Debug/net8.0/MockupRedoPeriod.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MockupRedoPeriod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2491419dd26a0d773ec94ed2c4fa6d61f8b3a051")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a586a7fefb29c69eff0936bb3c56f28bc577c0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MockupRedoPeriod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MockupRedoPeriod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
